90b64ccfe7332dd39b467ffb2be9a18247bf5321
Jack-WebDev <jackbuda31@gmail.com>
2025-10-26 11:48:27 +0200

    :lipstick: update loader

--- apps/web/src/components/loader.tsx
+++ apps/web/src/components/loader.tsx
@@ -2,8 +2,9 @@ import { Loader2 } from "lucide-react";
 
 export default function Loader() {
   return (
-    <div className="flex h-full items-center justify-center pt-8">
-      <Loader2 className="animate-spin" />
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-transparent text-white/90">
+      <Loader2 className="mb-3 h-8 w-8 animate-spin" />
+      <span className="font-medium text-base">Loading...</span>
     </div>
   );
 }
