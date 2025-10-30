7adfa7c36670f7d5ccaa30f6728651e86b16669a
Jack-WebDev <jackbuda31@gmail.com>
2025-10-30 17:20:36 +0200

    sst types

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -5,44 +5,45 @@
 
 declare module "sst" {
   export interface Resource {
-    Api: {
-      type: "sst.aws.ApiGatewayV2";
-      url: string;
-    };
-    Auth: {
-      type: "sst.sst.Secret";
-      value: string;
-    };
-    Bucket: {
-      name: string;
-      type: "sst.aws.Bucket";
-    };
-    Database: {
-      host: string;
-      name: string;
-      port: number;
-      type: "sst.sst.Linkable";
-      url: string;
-    };
-    DbMigrate: {
-      name: string;
-      type: "sst.aws.Function";
-    };
-    Network: {
-      publicSubnetIds: any;
-      securityGroupId: string;
-      type: "sst.sst.Linkable";
-      vpcId: string;
-    };
-    Vpc: {
-      type: "sst.aws.Vpc";
-    };
-    Web: {
-      type: "sst.aws.Nextjs";
-      url: string;
-    };
+    "Api": {
+      "type": "sst.aws.ApiGatewayV2"
+      "url": string
+    }
+    "Auth": {
+      "type": "sst.sst.Secret"
+      "value": string
+    }
+    "Bucket": {
+      "name": string
+      "type": "sst.aws.Bucket"
+    }
+    "Database": {
+      "host": string
+      "name": string
+      "port": number
+      "type": "sst.sst.Linkable"
+      "url": string
+    }
+    "DbMigrate": {
+      "name": string
+      "type": "sst.aws.Function"
+    }
+    "Network": {
+      "publicSubnetIds": any
+      "securityGroupId": string
+      "type": "sst.sst.Linkable"
+      "vpcId": string
+    }
+    "Vpc": {
+      "type": "sst.aws.Vpc"
+    }
+    "Web": {
+      "type": "sst.aws.Nextjs"
+      "url": string
+    }
   }
 }
 /// <reference path="sst-env.d.ts" />
 
-import "sst";
+import "sst"
+export {}
